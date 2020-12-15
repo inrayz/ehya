135ed682103b5db6b9383820b9a95c45ccd362a6
inrayz <inrayz@bk.ru>
2020-12-15 16:40:52 +0600

    /...

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var mySwiper = new Swiper('.reviews-slider', {
+  autoplay: {
+    delay: 7000,
+    disableOnInteraction: false, 
+  },
+  on: {
+    init() {
+      this.el.addEventListener('mouseenter', () => {
+        this.autoplay.stop();
+      });
+
+      this.el.addEventListener('mouseleave', () => {
+        this.autoplay.start();
+      });
+    }
+  },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+})
+
+
