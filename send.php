d52d8bd68e815690836e11dea9183c7d7a028989
inrayz <inrayz@bk.ru>
2020-12-17 08:28:59 +0600

    subscribe

---
+++ send.php
@@ -0,0 +1,54 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$email = $_POST['email'];
+
+
+// Формирование самого письма
+$title = "Новое сообщение Ehya";
+$body = "
+<h2>Новое письмо</h2>
+<b>Почта:</b> $email<br><br>";
+
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'inrayz@bk.ru'; // Логин на почте
+    $mail->Password   = 'ovv.2442'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('inrayz@bk.ru', 'Вячеслав Обрывалин'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('inrayz@yandex.ru'); 
+
+    
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
