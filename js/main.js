5003456fe07afb608d9d968b0f164b51813a2a9a
inrayz <inrayz@bk.ru>
2020-12-17 09:39:14 +0600

    php

--- js/main.js
+++ js/main.js
@@ -24,6 +24,18 @@ var mySwiper = new Swiper('.reviews-slider', {
     clickable: true,
   },
 })
+var mySwiper = new Swiper('.stories-swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.stories__button--next',
+    prevEl: '.stories__button--prev',
+  },
+})
+
 
 $(document).ready(function () {
   var trendingLink = $(".trending__link");
@@ -36,19 +48,18 @@ $(document).ready(function () {
     $(activeWrapper).addClass("card-wrapper__active");
     $(this).addClass("trending__link_active");
   });
+  $(".subscribe__form").validate({
+    errorClass: "invalid",
+     messages: {
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com"
+      },
+    },
+  });
 });
 
-var mySwiper = new Swiper('.stories-swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.stories__button--next',
-    prevEl: '.stories__button--prev',
-  },
-})
 
 
 
